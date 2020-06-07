e7c9de4700c6a19242eb5e745db3f18e70a400f5
masaya okawa <masamasa9841@gmail.com>
2020-06-07 21:19:02 +0900

    update path

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import * as functions from 'firebase-functions';
 
-const universal = require(`${process.cwd()}/dist/server/main.js`).app();
+const universal = require(`${process.cwd()}/functions/dist/server/main.js`).app();
 
 export const ssr = functions.https.onRequest(universal);
