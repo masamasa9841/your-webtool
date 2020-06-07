4e5b18d32be7cc0cb3578519cee2f5ed5cbe7134
Masaya Okawa <masamasa9841@gmail.com>
2020-06-07 21:54:40 +0900

    update yaml (#10)
    * :tada: add pwa (#1)
    
    * Feature/anagram (#2)
    
    * :tada: Add angular material
    
    * Add prettier
    
    * 一通り画面の作成
    
    * いらない文章の削除
    
    * test修正
    
    Co-authored-by: masaya okawa <masayaokawa@masayanoMacBook.local>
    
    * ホームページのタイトル追加
    
    * ssr追加
    
    * icon update
    
    * functions追加
    
    * deploy検証ずみのssr対応
    
    * githubci修正
    
    * fix githubactions
    
    * update ci
    
    * update yaml
    
    * update link
    
    * update path
    
    * update seo
    
    * update yaml
    
    * config修正
    
    * update gitignore
    
    Co-authored-by: masaya okawa <masayaokawa@masayanoMacBook.local>

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,16 @@ export interface WebTool {
   name: string;
   title: string;
   description: string;
+  keywords: string;
+  url: string;
 }
 
 export const WEB_TOOLS: WebTool[] = [
-  { name: 'anagram', title: 'アナグラム全パターン作成', description: '文字列から全パターンのアナグラムを作成します。' },
+  {
+    name: 'anagram',
+    title: 'アナグラム全パターン作成',
+    description: '文字列から全パターンのアナグラムを作成します。',
+    keywords: 'anagram, アナグラム, 順列, 全パターン, 作成',
+    url: 'http://localhost:4200/tools/anagram',
+  },
 ];
