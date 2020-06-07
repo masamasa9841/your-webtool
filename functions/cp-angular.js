c86f0388505f0a631414032ad95dd6a7cbb6c227
Masaya Okawa <masamasa9841@gmail.com>
2020-06-07 23:23:17 +0900

    ういｐ (#14)
    * :tada: add pwa (#1)
    
    * Feature/anagram (#2)
    
    * :tada: Add angular material
    
    * Add prettier
    
    * 一通り画面の作成
    
    * いらない文章の削除
    
    * test修正
    
    Co-authored-by: masaya okawa <masayaokawa@masayanoMacBook.local>
    
    * ホームページのタイトル追加
    
    * ssr追加
    
    * icon update
    
    * functions追加
    
    * deploy検証ずみのssr対応
    
    * githubci修正
    
    * fix githubactions
    
    * update ci
    
    * update yaml
    
    * update link
    
    * update path
    
    * update seo
    
    * update yaml
    
    * config修正
    
    * update gitignore
    
    * cp file mv
    
    * update path
    
    * update seo
    
    * backdrop解除
    
    * copy 削除
    
    Co-authored-by: masaya okawa <masayaokawa@masayanoMacBook.local>

--- functions/cp-angular.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs-extra');
-
-(async () => {
-  const src = `${process.cwd()}/../dist`;
-  const copy = `${process.cwd()}/../functions/dist`;
-
-  await fs.remove(copy);
-  await fs.copy(src, copy);
-})();
